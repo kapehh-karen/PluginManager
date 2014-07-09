a2f42fd9659e34ffff4002f568a955208d2623a9
kapehh-karen <git@kapehh.net>
2014-07-09 18:33:25 +0400

    added vault support

---
+++ src/me/kapehh/main/pluginmanager/vault/PluginVault.java
@@ -0,0 +1,37 @@
+package me.kapehh.main.pluginmanager.vault;
+
+import net.milkbowl.vault.chat.Chat;
+import net.milkbowl.vault.economy.Economy;
+import net.milkbowl.vault.permission.Permission;
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.RegisteredServiceProvider;
+
+/**
+ * Created by Karen on 09.07.2014.
+ */
+public class PluginVault {
+
+    public static Permission setupPermissions() {
+        RegisteredServiceProvider<Permission> permissionProvider = Bukkit.getServer().getServicesManager().getRegistration(net.milkbowl.vault.permission.Permission.class);
+        if (permissionProvider != null) {
+            return permissionProvider.getProvider();
+        }
+        return null;
+    }
+
+    public static Chat setupChat() {
+        RegisteredServiceProvider<Chat> chatProvider = Bukkit.getServer().getServicesManager().getRegistration(net.milkbowl.vault.chat.Chat.class);
+        if (chatProvider != null) {
+            return chatProvider.getProvider();
+        }
+        return null;
+    }
+
+    public static Economy setupEconomy() {
+        RegisteredServiceProvider<Economy> economyProvider = Bukkit.getServer().getServicesManager().getRegistration(net.milkbowl.vault.economy.Economy.class);
+        if (economyProvider != null) {
+            return economyProvider.getProvider();
+        }
+        return null;
+    }
+}
