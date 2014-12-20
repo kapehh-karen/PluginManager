8b6dae1f34190e3725a21ca6d2b188db33041f9a
kapehh <git@kapehh.net>
2014-12-20 16:34:22 +0300

    add const

--- src/me/kapehh/main/pluginmanager/constants/ConstantSystem.java
+++ src/me/kapehh/main/pluginmanager/constants/ConstantSystem.java
@@ -6,4 +6,5 @@ package me.kapehh.main.pluginmanager.constants;
 public class ConstantSystem {
     public static final String fileSep = System.getProperty("file.separator");
     public static final String lineSep = System.getProperty("line.separator");
+    public static final int ticksPerSec = 20;
 }
