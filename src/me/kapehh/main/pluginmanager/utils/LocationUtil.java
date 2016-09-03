afb13757a19485327bd291fef197caef4016ed87
kapehh-karen <git@kapehh.net>
2016-09-03 21:48:25 +0300

    add megasuperduper method isLocationPVP
    isLocationPVP required Towny, WE, WG

--- src/me/kapehh/main/pluginmanager/utils/LocationUtil.java
+++ src/me/kapehh/main/pluginmanager/utils/LocationUtil.java
@@ -1,5 +1,13 @@
 package me.kapehh.main.pluginmanager.utils;
 
+import com.palmergames.bukkit.towny.exceptions.NotRegisteredException;
+import com.palmergames.bukkit.towny.object.Coord;
+import com.palmergames.bukkit.towny.object.WorldCoord;
+import com.sk89q.worldguard.bukkit.WGBukkit;
+import com.sk89q.worldguard.protection.flags.DefaultFlag;
+import com.sk89q.worldguard.protection.flags.StateFlag;
+import com.sk89q.worldguard.protection.managers.RegionManager;
+import com.sk89q.worldguard.protection.regions.ProtectedRegion;
 import org.bukkit.Location;
 
 /**
@@ -13,6 +21,57 @@ public class LocationUtil {
             return false;
         }
         return (pos1.distance(pos2) <= distance);
+    }
+
+    // Required: Towny, WorldEdit, WorldGuard
+    public static boolean isLocationPVP(Location location) {
+        RegionManager regionManager = WGBukkit.getPlugin().getRegionManager(location.getWorld());
+        StateFlag.State globalPVP = regionManager.getRegion("__global__").getFlag(DefaultFlag.PVP);
+        boolean isGlobalPVP = StateFlag.State.ALLOW.equals(globalPVP);
+
+        //System.out.println("Global PVP: " + isGlobalPVP);
+
+        boolean isContainsRegions = false;
+        boolean isRegionPVP = true;
+        for (ProtectedRegion protectedRegion : regionManager.getApplicableRegions(location).getRegions()) {
+            StateFlag.State pvp = protectedRegion.getFlag(DefaultFlag.PVP);
+            isRegionPVP &= StateFlag.State.ALLOW.equals(pvp);
+            isContainsRegions = true;
+        }
+
+        //System.out.println("Regions PVP: " + isRegionPVP);
+
+        boolean isWorldGuardPVP = isContainsRegions ? isRegionPVP : isGlobalPVP;
+
+        //System.out.println("IS WG PVP: " + isWorldGuardPVP);
+
+        WorldCoord worldCoord = new WorldCoord(location.getWorld().getName(), Coord.parseCoord(location));
+        boolean isTownyWorldPVP = false;
+        try {
+            isTownyWorldPVP = worldCoord.getTownyWorld().isPVP() || worldCoord.getTownyWorld().isForcePVP();
+        } catch (NotRegisteredException e) {
+
+        }
+
+        //System.out.println("isTownyWorldPVP: " + isTownyWorldPVP);
+
+        boolean isTownyPlot = false;
+        boolean isTownyPlotPVP = false;
+        try {
+            isTownyPlot = (worldCoord.getTownBlock().getTown() != null);
+            isTownyPlotPVP = worldCoord.getTownBlock().getPermissions().pvp;
+        } catch (NotRegisteredException e) {
+
+        }
+
+        //System.out.println("isTownyPlot: " + isTownyPlot + ", isTownyPlotPVP: " + isTownyPlotPVP);
+
+        boolean isTownyPVP = (isTownyPlot && isTownyPlotPVP) || (!isTownyPlot && isTownyWorldPVP);
+
+        //System.out.println("IS TOWNY PVP: " + isTownyPVP);
+
+        // Если и то и это разрешают, то почему бы и нет
+        return isTownyPVP && isWorldGuardPVP;
     }
 
 }
